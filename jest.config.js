4a8427dbd68719bc45d06766cc9740442597f20a
Yoursuperherobasu <basudps@gmail.com>
2026-05-09 19:18:56 +0530

    Remove legacy node-runner test/logic.test.js (superseded by jest)

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'jest-expo',
   setupFiles: ['<rootDir>/jest.setup.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/.expo/', '/test/logic.test.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/.expo/'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg))',
   ],
